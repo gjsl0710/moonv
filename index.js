ff7fb268ea2452ed41716bf07f344ad1f82e8a90
gjsl0710 <gjsl070@ruu.kr>
2020-11-19 14:10:50 +0900

    add

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.token;
-const prefix = "/"
 
 client.on('ready', () => {
     console.log('DISCORD BOT LOGIN');
